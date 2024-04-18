cc25d8d895fee51da1cb51f61f058494bf2edfb3
rir7890 <Rahul Kumar>
2024-04-18 17:19:16 +0530

    mongodb connect through import

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import connectDB from "./db/index.js";
+import dotenv from "dotenv";
+dotenv.config();
+
+/*
+////first approach
+import express from "express";
+const app = express();
+
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+    app.on("error", () => {
+      console.log("Error: ", error);
+      throw error;
+    });
+    app.listen(process.env.PORT, () => {
+      console.log(`App is listening on ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.error("error :", error);
+    throw err;
+  }
+})();
+*/
+
+connectDB();
